fea781d8a6000a86367aa9dc0da23b3bc17c9bd8
Edgar Hernández <23639005+israel-hdez@users.noreply.github.com>
2024-02-19 14:50:24 -0600

    Add Slack in my default desktop apps set

--- scripts/install-desktop-apps.sh
+++ scripts/install-desktop-apps.sh
@@ -4,7 +4,7 @@
 set -x
 
 if [ "$DISTRO" == "fedora" ]; then
-  sudo dnf install -y remmina gimp gnucash
+  sudo dnf install -y remmina gimp gnucash slack
   sudo dnf install -y --setopt=install_weak_deps=False pass oathtool pwgen zbar
 fi
 
